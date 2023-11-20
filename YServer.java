1673450d011e551e01747b89dbe06bc7de9b6427
Mohammad El Kadri <mae126@mail.aub.edu>
2023-11-20 18:05:15 +0200

    server code

--- YServer.java
+++ YServer.java
@@ -4,20 +4,30 @@ import java.net.Socket;
 
 public class YServer {
     public static void main(String[] args) {
-        int port = Integer.parseInt(args[0]); // Get port number from command line argument
+        // Check if the port number is provided
+        if (args.length < 1) {
+            System.out.println("Usage: java YServer <port number>");
+            return;
+        }
+
+        // Parse the port number from the command line argument
+        int port = Integer.parseInt(args[0]);
 
         // Database connection details
-        String jdbcURL = "jdbc:mysql://localhost/your_database_name";
-        String jdbcUsername = "your_username";
-        String jdbcPassword = "your_password";
+        String jdbcURL = "jdbc:mysql://localhost/Y:3306";
+        String jdbcUsername = "root";
+        String jdbcPassword = "kali";
 
+        // Create an instance of UserManager
         UserManager userManager = new UserManager(jdbcURL, jdbcUsername, jdbcPassword);
 
+        // Start the server and listen for client connections
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Server is listening on port " + port);
             while (true) {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("New client connected");
+                // Create a new thread for each client
                 new ClientHandler(clientSocket, userManager).start();
             }
         } catch (IOException e) {
